aec429b9a833157ab7fe10569d521c045293891b
Jack Weinbender <jack.weinbender@gmail.com>
2017-02-05 11:20:56 -0600

    First half correct

--- day04/day04.rb
+++ day04/day04.rb
@@ -2,17 +2,38 @@ class Room
     def initialize ( str )
         *@name, @sector = str.split("[")[0].strip.split("-")
         @checksum = str[/.*\[(.*)\]/, 1].strip
-        puts "INITIALIZED:\n- name: '#{@name}'\n- cksm: '#{@checksum}'\n- id: '#{@sector}'"
+        #puts "INITIALIZED:\n- name: '#{@name}'\n- cksm: '#{@checksum}'\n- id: '#{@sector}'"
     end
-    def summarize()
-        name_normalized = @name.join("")
+    def get_sector()
+        @sector.to_i
     end
     
+    def summarize()
+        name_normalized = @name.join("").split("").sort
+        name_hash = name_normalized
+            .reduce({}) do |acc, name|
+                acc.merge({name => 1}){|key, oldval, newval| newval + oldval}
+            end
+        
+        Hash[name_hash.sort_by{|k, v| [v*-1, k]}].keys.shift(5).join("")
+    end
+    def is_valid()
+        if summarize() == @checksum then
+            puts "#{summarize()} == #{@checksum}"
+            true
+        else
+            puts "#{summarize()} != #{@checksum}"
+            false
+        end
+        
+    end
 end
 
 
-input = File.readlines("_test_data_1514.txt")
+# total = File.readlines("_test_data_1514.txt")
+total = File.readlines("_data.txt")
+    .map { |line| Room.new(line) }
+    .select { |room| room.is_valid() }
+    .reduce(0) {|acc, room| acc + room.get_sector()}
 
-for line in input
-    Room.new(line)
-end
+puts "Total: #{total}"
