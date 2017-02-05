578e419f97f6f6fe56883aa9b7c22bf0eb528c27
Jack Weinbender <jack.weinbender@gmail.com>
2017-02-05 11:23:36 -0600

    First half correct

--- day04/day04.rb
+++ day04/day04.rb
@@ -30,10 +30,11 @@ class Room
 end
 
 
-# total = File.readlines("_test_data_1514.txt")
-total = File.readlines("_data.txt")
+valid_rooms = File.readlines("_data.txt")
+valid_rooms = File.readlines("_test_data_1514.txt")
     .map { |line| Room.new(line) }
     .select { |room| room.is_valid() }
-    .reduce(0) {|acc, room| acc + room.get_sector()}
+
+total = valid_rooms.reduce(0) {|acc, room| acc + room.get_sector()}
 
 puts "Total: #{total}"
