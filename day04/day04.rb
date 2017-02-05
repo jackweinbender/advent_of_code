622c19549f20cc4edb55a6536aa96369d99056ba
Jack Weinbender <jack.weinbender@gmail.com>
2017-02-05 12:55:58 -0600

    Correct second answer

--- day04/day04.rb
+++ day04/day04.rb
@@ -19,22 +19,48 @@ class Room
     end
     def is_valid()
         if summarize() == @checksum then
-            puts "#{summarize()} == #{@checksum}"
+            # puts "#{summarize()} == #{@checksum}"
             true
         else
-            puts "#{summarize()} != #{@checksum}"
+            # puts "#{summarize()} != #{@checksum}"
             false
         end
         
     end
+    def decode()
+        name = @name.map do | word |
+            word.split("").map { |ch| char_shift(ch) }.join("")
+        end
+
+        name.join(" ")
+    end
+    def char_shift( char )
+        c = ("a".."z").to_a
+        mod = get_sector() % 26
+        pos = c.index(char)
+
+        index = (pos + mod) % 26
+        c[index]
+    end
+
 end
 
+puts Room.new("qzmt-zixmtkozy-ivhz-343[zimth]").decode()
 
-valid_rooms = File.readlines("_data.txt")
 valid_rooms = File.readlines("_test_data_1514.txt")
+valid_rooms = File.readlines("_test_data_2.txt")
+valid_rooms = File.readlines("_data.txt")
     .map { |line| Room.new(line) }
     .select { |room| room.is_valid() }
 
 total = valid_rooms.reduce(0) {|acc, room| acc + room.get_sector()}
 
-puts "Total: #{total}"
+q = "very encrypted name"
+q = "North Pole"
+
+regex = q.downcase.split(" ").join(".*")
+
+first_match = valid_rooms.select { |room| room.decode().match(/#{regex}/)  }.first
+
+puts "Sector total of valid rooms: #{total}"
+puts "First sector match for '#{q}' is '#{first_match.get_sector()}'"
