2aa000891630397e8dbe7953089106bc3224e999
Jack Weinbender <jack.weinbender@gmail.com>
2017-02-06 21:08:46 -0600

    Parts 1 & 2

--- day06/day06.rb
+++ day06/day06.rb
@@ -1,5 +1,27 @@
 
-
-
+code = Array.new(8, {})
 
 input = File.readlines("_test_data_easter.txt")
+input = File.readlines("_data.txt")
+    .map { |line| line.strip }
+    .map { |line| line.split("").map { |ch| { ch => 1 } } }
+    .reduce(code) {
+        |acc, line|
+        #puts "#{acc} || #{line}"
+        for i in 0..7
+            acc[i] = acc[i].merge(line[i]) { |k, o, n| o + n }
+            #puts "#{acc}"
+        end
+        acc
+    }.map { |hash| 
+        # Part 1
+        hash.max_by { |k, v| v } 
+        # Part 2
+        # hash.min_by { |k, v| v } 
+    }
+    .map { |hash| hash[0] }
+    .join("")
+
+puts "#{input}"
+
+# qtbjqi
