cb02fe417e8ddf7d087e25c996551b0e31686191
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-04 22:41:31 -0500

    Passes first half

--- day07/day07.rb
+++ day07/day07.rb
@@ -1,20 +1,29 @@
 
 class IPV
     def initialize( str )
-        @head, @brackets, @tail = from_string(str)
-        @valid_ipv = valid_ipv(from_string(str))
+        @hypernets = hypernets_from_string(str)
+        @non_hypernets = non_hypernets_from_string(str)
+        @valid_ipv = valid_ipv(@hypernets, @non_hypernets)
     end
-    def from_string (str)
-        str.split(/[\[\]]/)
+    def hypernets_from_string (str)
+        hyps = str.split(/[\[\]]/)
+        (0..hyps.length).select { |i| i.odd? }
+            .map { |i| hyps[i] }
+            .select { |i| i != nil }
+    end
+    def non_hypernets_from_string (str)
+        hyps = str.split(/[\[\]]/)
+        (0..hyps.length).select { |i| i.even? }
+            .map { |i| hyps[i] }
     end
     def contains_abba?( str )
     str.scan(/([a-zA-Z])([^\1])\2\1/)
         .any? { |m| m != nil && m[0] != m[1] }
     end
-    def valid_ipv( arr )
-        if contains_abba?(arr[1]) then
+    def valid_ipv( hypernets_arr, non_hypernets_arr )
+        if hypernets_arr.any? { |h| contains_abba?(h) }  then
             false
-        elsif contains_abba?(arr[0]) || contains_abba?(arr[2])
+        elsif non_hypernets_arr.any? { |h| contains_abba?(h) }
             true
         else
             false
@@ -23,14 +32,10 @@ class IPV
     def valid?
         @valid_ipv
     end
-    def to_string
-        puts "#{@head}[#{@brackets}]#{@tail}"
-    end
     
 end
 
-puts input = File.readlines("_test_data.txt")
+puts input = File.readlines("_data.txt")
     .map { |line| IPV.new(line) }
     .select { |ipv| ipv.valid? }
-    .map { |ipv| ipv.to_string() }
     .length
