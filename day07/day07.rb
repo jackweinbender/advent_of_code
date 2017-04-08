59877e8b5b15587efe6206f31af13dd00b018379
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-07 22:17:42 -0500

    Answer 2

--- day07/day07.rb
+++ day07/day07.rb
@@ -1,9 +1,10 @@
 # http://adventofcode.com/2016/day/7
+require 'json'
 
 class IPV
     def initialize( str )
         @hypernets = hypernets_from_string(str)
-        @supernets = non_hypernets_from_string(str)
+        @supernets = supernets_from_string(str)
     end
     def hypernets_from_string (str)
         hyps = str.split(/[\[\]]/)
@@ -11,14 +12,28 @@ class IPV
             .map { |i| hyps[i] }
             .select { |i| i != nil }
     end
-    def non_hypernets_from_string (str)
-        hyps = str.split(/[\[\]]/)
-        (0..hyps.length).select { |i| i.even? }
-            .map { |i| hyps[i] }
+    def supernets_from_string (str)
+        sups = str.split(/[\[\]]/)
+        (0..sups.length).select { |i| i.even? }
+            .map { |i| sups[i] }
+            .select { |i| i != nil }
     end
     def contains_abba?( str )
-    str.scan(/([a-zA-Z])([^\1])\2\1/)
-        .any? { |m| m != nil && m[0] != m[1] }
+    str.scan(/(\w)(\w)\2\1/)
+        .any? { |m| m != nil && m[-1] != m[-2] }
+    end
+    def get_abas( arr )
+        arr.map { |str| str
+            out = []
+            max = str.length - 2;
+            for i in 0..max
+                if str[i] == str[i + 2] && str[i] != str[i+1] then
+                    # puts "#{str[i]}#{str[i+1]}#{str[i]}"
+                    out.push("#{str[i]}#{str[i+1]}#{str[i]}")
+                end
+            end
+            out
+        }.flatten()
     end
     def valid_ipv?
         if @hypernets.any? { |h| contains_abba?(h) }  then
@@ -29,10 +44,26 @@ class IPV
             false
         end
     end
-    
+    def valid_ssl?
+        abas = get_abas(@supernets)
+        babs = get_abas(@hypernets)
+            .map {|aba| "#{aba[1]}#{aba[0]}#{aba[1]}" }
+        abas.any? { |aba| babs.include? aba }
+    end
 end
 
-puts input = File.readlines("_data.txt")
+# # Answer #1
+ans1 = File.readlines("_data.txt")
     .map { |line| IPV.new(line) }
     .select { |ipv| ipv.valid_ipv? }
     .length
+puts "Answer 1: #{ans1}"
+
+# # Answer #2
+
+ans2 = File.readlines("_data.txt")
+    .map { |line| IPV.new(line) }
+    .select { |ipv| ipv.valid_ssl? }
+    .length
+
+puts "Answer 2: #{ans2}"
