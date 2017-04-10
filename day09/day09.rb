b1b73eb198e344d529af20c8ab62d0b84761e5ec
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-10 11:00:38 -0500

    Both Answers

--- day09/day09.rb
+++ day09/day09.rb
@@ -4,18 +4,82 @@ class Parser
     def initialize()
     end
     def parse( str )
-        m = str.match(/\((\d+)x(\d+)\)/)
-        puts "#{m}"
+        output = ''
+        i = 0
+        while i < str.length
+            case str[i]
+            when "("
+                match = str.match(/\d+x\d+/, i+1)[0]
+                len, mag = match.split("x").map(&:to_i)
+                chunk_index = i + match.length + 2
+
+                chunk = str.slice( chunk_index, len)
+
+                mag.times { output << chunk }
+
+                i = chunk_index + len
+            else
+                output << str[i]
+                i += 1
+            end
+        end
+        output
     end
-    def parsed_len(str)
-        return parse( str ).length
+    def parse_v2_len( str )
+        output = 0
+        i = 0
+        while i < str.length
+            case str[i]
+            when "("
+                match = str.match(/\d+x\d+/, i+1)[0]
+                len, mag = match.split("x").map(&:to_i)
+                chunk_index = i + match.length + 2
+
+                chunk_len = parse_v2_len(str.slice( chunk_index, len))
+                # puts chunk_len
+                mag.times { output += chunk_len }
+
+                i = chunk_index + len
+            else
+                output += 1
+                i += 1
+            end
+        end
+        output
     end
 end
 
+
+## V1
 str = File.readlines('_data.txt')
     .map{ |l| l.chomp }.join('')
-
 p = Parser.new()
-puts p.parse(str)
+puts "V1: #{p.parse( str ).length}"
 
-# Starts @ 14248
+# test_data = [
+#     ["ADVENT", 6],
+#     ["A(1x5)BC", 7]
+# ]
+
+# p = Parser.new()
+# test_data.each do |test|
+#     puts p.parse( test[0] ).length == test[1]
+# end
+
+## V2
+str = File.readlines('_data.txt')
+    .map{ |l| l.chomp }.join('')
+p = Parser.new()
+puts "V2: #{p.parse_v2_len( str )}"
+
+# test_data = [
+#     ["(3x3)XYZ", 9],
+#     ["X(8x2)(3x3)ABCY", 20],
+#     ["(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920],
+#     ["(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445]
+# ]
+
+# p = Parser.new()
+# test_data.each do |test|
+#     puts p.parse_v2_len( test[0] ) == test[1]
+# end
