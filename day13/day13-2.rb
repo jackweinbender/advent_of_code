f6a5d8b3d6b1cbedd943a15012d612704b9ab5de
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-28 22:55:23 -0500

    Answer #2

---
+++ day13/day13-2.rb
@@ -0,0 +1,68 @@
+class State
+    attr_accessor :steps
+    def initialize(state, num, goal, steps)
+        @state = state
+        @number = num
+        @goal = goal
+        @steps = steps
+    end
+    def explode
+        combos = []
+        combos.push([@state[0], @state[1] + 1])
+        combos.push([@state[0] + 1, @state[1]])
+        combos.push([@state[0], @state[1] - 1])
+        combos.push([@state[0] - 1, @state[1]])
+
+        combos.reject { |x, y| x < 0 || y < 0 }
+            .select { |x, y| 
+                # puts "x: #{x}, y: #{y}, #{open?(x, y, @number)}"
+                open?(x, y, @number)
+            }
+            .map { |state|
+                State.new(state, @number, @goal, @steps + 1)
+            }        
+    end
+    def open?(x, y, n)
+        integer = x*x + 3*x + 2*x*y + y + y*y + @number
+        open = integer.to_s(2).split('')
+            .reject { |i| i == "0" }
+            .count % 2
+        open == 1 ? false : true
+    end
+    def complete?
+        @state == @goal
+    end
+    def priority
+        steps + hueristic(@state, @goal)
+    end
+    def hueristic(state, goal)
+        (goal[0] - state [0]) + (goal[1] - state[1])
+    end
+    def to_s
+        "#{@state}"
+    end
+end
+
+init = [1,1]
+num = 1358
+goal = [31,39]
+
+queue = [] 
+visited = []
+
+queue << State.new(init, num, goal, 0)
+i = 1
+while not queue.empty?
+
+    state = queue.shift
+
+    state.explode.each do |new_state|
+        next if visited.include?(new_state.to_s)
+        next if new_state.steps > 50
+        # puts "#{new_state.to_s}"
+        queue << new_state
+        visited << new_state.to_s
+    end
+end
+
+puts "Visited #{visited.count} squares."
