0469357e904e0a1b9fca72c16fc31b46a754df8c
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-30 10:01:10 -0500

    Answer 1

---
+++ day16/day16-1.rb
@@ -0,0 +1,33 @@
+
+def dragonize(string, length)
+    puts string
+    string if length == string.length
+    if length <  string.length
+        string[0, length]
+    else
+        reversed = string.reverse
+        string << "0"
+        string << reversed.chars.map { |c| c == "0" ? "1" : "0" }.join('')
+        dragonize(string, length)
+    end
+end
+
+def get_checksum(string)
+    while string.length % 2 == 0
+        puts string
+        string = string.scan(/.{2}/).map do |pair|
+            pair[0] == pair[1] ? "1" : "0"
+        end.join
+    end
+    string
+end
+
+# test = dragonize("10000", 20)
+# puts "Test passed? #{get_checksum(test) == "01100"}"
+
+# Checksum: 11100110111101110
+input = "10111100110001111"
+length = 272
+
+data = dragonize(input, length)
+puts "Checksum: #{get_checksum(data)}"
