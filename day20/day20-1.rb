1b5b10e272f553f91cd020eeebfdeaaf3f351f25
Jack Weinbender <jack.weinbender@gmail.com>
2017-05-02 21:54:53 -0500

    answer #1

--- day20/day20-1.rb
+++ day20/day20-1.rb
@@ -1,4 +1,19 @@
 # 0 - 4294967295
 
 cieling = 4294967295
-    
+
+blacklist = File.readlines('input.txt')
+    .map { |b| b.split('-').map(&:to_i) }
+    .sort { |a,b| a[0] <=> b[0] }
+
+fr_max = 0
+for bl in blacklist
+    if bl[0] <= fr_max
+        fr_max = [bl[1], fr_max].max
+        next
+    end
+    puts "First IP: #{fr_max+1}"
+    break
+end
+
+# 17348574
