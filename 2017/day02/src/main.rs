43cc9b9d75b2475a3ef7720a863e7e546054d8cf
Jack Weinbender <jack.weinbender@gmail.com>
2017-12-14 18:23:05 -0600

    Day 2 Complete

--- 2017/day02/src/main.rs
+++ 2017/day02/src/main.rs
@@ -1,16 +1,15 @@
 fn main() {
     let input = include_str!("input.txt");
-    println!("Answer #1: {}", checksum(input));
+    println!("Answer #1: {}", sum_checksum(input));
+    println!("Answer #2: {}", div_checksum(input));
 }
 
-fn checksum(input: &str) -> i32 {
+fn sum_checksum(input: &str) -> i32 {
     let mut checksum = 0;
     let mut lines = input.lines();
 
     while let Some(l) = lines.next() {
-        let mut cells:Vec<&str> = l.split_whitespace().collect();
-
-        let cells:Vec<i32> = cells.into_iter()
+        let cells:Vec<i32> = l.split_whitespace()
             .map(|x| i32::from_str_radix(x, 10).unwrap() ).collect();
         
         let min = cells.iter().cloned().min().unwrap();
@@ -20,15 +19,43 @@ fn checksum(input: &str) -> i32 {
     }
     checksum
 }
+fn div_checksum(input: &str) -> i32 {
+    let mut checksum = 0;
+    let mut lines = input.lines();
 
+    while let Some(l) = lines.next() {
+        let cells:Vec<i32> = l.split_whitespace()
+            .map(|x| i32::from_str_radix(x, 10).unwrap() ).collect();
+        
+        for (i, cell) in cells.iter().cloned().enumerate() {
+            for cmp in cells.iter().skip(i+1){
+                if cell % cmp == 0 { 
+                    checksum += cell / cmp;
+                    break;
+                }
+                if cmp % cell == 0 { 
+                    checksum += cmp / cell;
+                    break;
+                }
+            }
+        }
+    }
+    checksum
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_checksum() {
-        let input = include_str!("test_input.txt");
+    fn test_sum_checksum() {
+        let input = include_str!("test_input_1.txt");
 
-        assert_eq!(checksum(input), 18);
+        assert_eq!(sum_checksum(input), 18);
+    }
+    #[test]
+    fn test_div_checksum() {
+        let input = include_str!("test_input_2.txt");
+
+        assert_eq!(div_checksum(input), 9);
     }
 }
