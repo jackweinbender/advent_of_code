739aca145245d1b4ee45a88e951eda7909446931
Jack Weinbender <jack.weinbender@gmail.com>
2017-12-15 09:18:35 -0600

    Formatting

--- 2017/day04/src/main.rs
+++ 2017/day04/src/main.rs
@@ -1,23 +1,27 @@
 fn main() {
     let input = include_str!("input.txt");
-    
-    let valid_pwds:Vec<&str> = input.lines().filter(|x| is_valid(x)).collect();
+
+    let valid_pwds: Vec<&str> = input.lines().filter(|x| is_valid(x)).collect();
 
     println!("Answer #1: {}", valid_pwds.len());
 }
 
 fn is_valid(phrase: &str) -> bool {
-    
-    let mut has_dupes:bool = false;
-
+    let mut has_dupes: bool = false;
     let mut words: Vec<&str> = phrase.split_whitespace().collect();
-    words.sort();
+        words.sort();
+    
     let mut sorted = words.into_iter().peekable();
 
     while let Some(w) = sorted.next() {
         match sorted.peek() {
-            Some(x) if &w == x => { has_dupes = true; break; }
-            _ => { continue; }
+            Some(x) if &w == x => {
+                has_dupes = true;
+                break;
+            }
+            _ => {
+                continue;
+            }
         }
     }
     !has_dupes
