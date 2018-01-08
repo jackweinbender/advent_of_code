211996b4da02668b00c72a4a7501a591e143218f
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-08 16:33:44 -0600

    Basic setup with tests

--- 2017/day09/src/main.rs
+++ 2017/day09/src/main.rs
@@ -1,3 +1,28 @@
+
+type Score = usize;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+}
+
+fn score(input: &str) -> Score {
+    unimplemented!();
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_score(){
+        assert_eq!(1, score("{}"));
+        assert_eq!(6, score("{{{}}}"));
+        assert_eq!(5, score("{{},{}}"));
+        assert_eq!(16, score("{{{},{},{{}}}}"));
+        assert_eq!(1, score("{<a>,<a>,<a>,<a>}"));
+        assert_eq!(9, score("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
+        assert_eq!(9, score("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
+        assert_eq!(3, score("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+    }
 }
