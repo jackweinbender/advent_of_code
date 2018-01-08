9a49180366e70c20af11b727144f0bffbabf1243
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-08 17:06:28 -0600

    Day 08 Complete

--- 2017/day09/src/main.rs
+++ 2017/day09/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Answer #1: {}", score(input));
+    println!("Answer #2: {}", score_skipped(input));
 }
 
 fn score(input: &str) -> Score {
@@ -41,6 +42,33 @@ fn score(input: &str) -> Score {
     total
 }
 
+fn score_skipped(input: &str) -> Score {
+    let mut input_iter = input.chars();
+    
+    let mut total_skipped = 0 as Score;
+    let mut state = Parser::Stream;
+
+    while let Some(ch) = input_iter.next() {
+        match state {
+            Parser::Stream => {
+                match ch {
+                    '<' => { state = Parser::Garbage; }
+                    _ => {} // Proceed
+                }
+            },
+            Parser::Garbage => {
+                match ch {
+                    '>' => { state = Parser::Stream; }
+                    '!' => { state = Parser::Skip; }
+                    _ => { total_skipped += 1; } // Proceed
+                }
+            },
+            Parser::Skip => { state = Parser::Garbage }
+        }
+    }
+    total_skipped
+}
+
 #[derive(Debug,PartialEq)]
 enum Parser {
     Stream,
@@ -62,5 +90,15 @@ mod test {
         assert_eq!(9, score("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
         assert_eq!(9, score("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
         assert_eq!(3, score("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+    }
+    #[test]
+    fn test_score_skipped(){
+        assert_eq!(0, score_skipped("<>"));
+        assert_eq!(17, score_skipped("<random characters>"));
+        assert_eq!(3, score_skipped("<<<<>"));
+        assert_eq!(2, score_skipped("<{!>}>"));
+        assert_eq!(0, score_skipped("<!!>"));
+        assert_eq!(0, score_skipped("<!!!>>"));
+        assert_eq!(10, score_skipped("<{o\"i!a,<{i<a>"));
     }
 }
