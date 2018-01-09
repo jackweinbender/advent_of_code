39a8d464366f9234ca1b7dbfffaed579e8f73611
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-09 11:53:25 -0600

    Answer #1

--- 2017/day10/src/main.rs
+++ 2017/day10/src/main.rs
@@ -1,26 +1,51 @@
 #![feature(slice_rotate)]
 
 type Knot = Vec<Increment>;
+type Length = usize;
 type Increment = usize;
 type Skip = usize;
 type Index = usize;
 
 fn main() {
-    let input = "46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204";
+    let lengths: Vec<Length> = "46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204"
+        .split(',')
+        .map(|x| usize::from_str_radix(x, 10).unwrap())
+        .collect();
+    let mut knot: Knot = (0..256).collect();
 
-    println!("Answer #1: {:?}", tie_knot(input).into_iter().take(2).collect::<Vec<Increment>>());
+    tie_knot(&mut knot, lengths);
+
+    println!("Answer #1: {:?}", knot_product(knot));
 
 }
 
-fn tie_knot(input: &str) -> Knot {
-    let mut knot: Knot = (0..256).collect();
+fn tie_knot(knot: &mut Knot, lengths: Vec<Length>) -> () {
     let mut skip: Skip = 0;
     let mut index: Index = 0;
+    let knot_len = knot.len();
 
-    knot
+    for length in lengths {
+        if length > knot_len {
+            continue;
+        }
+
+        reverse_slice(index, length, knot);
+
+        index += length;
+        index += skip;
+        index %= knot_len;
+
+        skip += 1;
+    }
+}
+
+fn knot_product(knot: Knot) -> isize {
+    knot.clone().into_iter().take(2).fold(1,|acc, x| acc * x as isize)
 }
 
 fn reverse_slice(index: Index, length: Increment, knot: &mut Knot) -> () {
+    let knot_length = knot.len();
+
     knot.rotate(index);
 
     {
@@ -28,6 +53,23 @@ fn reverse_slice(index: Index, length: Increment, knot: &mut Knot) -> () {
         z.reverse();
     }
 
-    knot.rotate(5 - index);
-    println!("{:?}", knot);
+    knot.rotate(knot_length - index);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_hash_knot() {
+        let lengths = "3,4,1,5"
+            .split(',')
+            .map(|x| usize::from_str_radix(x, 10).unwrap())
+            .collect();
+        let mut knot: Knot = (0..5).collect();
+
+        tie_knot(&mut knot, lengths);
+
+        assert_eq!(vec![3, 4, 2, 1, 0], knot);
+        assert_eq!(12, knot_product(knot));
+    }
 }
