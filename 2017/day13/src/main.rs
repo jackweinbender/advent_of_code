cdf459c694bc7076b5e36cc07ebe7859e02a68fd
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-11 14:26:43 -0600

    Answer #2

--- 2017/day13/src/main.rs
+++ 2017/day13/src/main.rs
@@ -5,29 +5,52 @@ type Severity = usize;
 type Depth = usize;
 type Range = usize;
 type Time = usize;
+type Trip = Vec<Scanner>;
 
 fn main() {
     let input = include_str!("input.txt");
 
-    let trip_severity: usize = trip_severity(input).iter().sum();
-    println!("Answer #1: {:?}", trip_severity);
+    let trip = get_trip(input);
+    println!("Answer #1: {:?}", trip_severity(&trip, 0));
+    println!("Answer #2: {:?}", shortest_delay(&trip));
 }
 
-fn trip_severity(input: &str) -> Vec<Severity> {
+fn shortest_delay(trip: &Trip) -> Time {
+    let mut offset = 0;
+    loop {
+        if is_clean_trip(trip, offset) {
+            return offset;
+        } else {
+            offset += 1
+        } // Do nothing
+    }
+}
+
+fn trip_severity(trip: &Trip, offset: Time) -> Severity {
+    trip.iter().filter_map(|x| x.caught_at(x.0 + offset)).sum()
+}
+
+fn is_clean_trip(trip: &Trip, offset: Time) -> bool {
+    trip.iter().map(|x| x.caught_at(x.0 + offset)).all(
+        |x| x == None,
+    )
+}
+
+fn get_trip(input: &str) -> Trip {
     input
         .lines()
         .map(|x| x.parse::<Scanner>().unwrap())
-        .map(|x| x.caught_at(x.0))
         .collect()
 }
 
+#[derive(Debug)]
 struct Scanner(Depth, Range);
 impl Scanner {
-    fn caught_at(&self, time: Time) -> Severity {
+    fn caught_at(&self, time: Time) -> Option<Severity> {
         let full_range = (2 * self.1) - 2;
         match time % full_range {
-            0 => self.0 * self.1,
-            _ => 0,
+            0 => Some(self.0 * self.1),
+            _ => None,
         }
     }
 }
@@ -51,7 +74,8 @@ mod tests {
     fn test_severity() {
         let input = include_str!("test_input.txt");
 
-        let trip_severity: usize = trip_severity(input).iter().sum();
-        assert_eq!(24, trip_severity);
+        let trip = get_trip(input);
+        assert_eq!(24, trip_severity(&trip, 0));
+        assert_eq!(10, shortest_delay(&trip));
     }
 }
