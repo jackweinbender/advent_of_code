8aa9250db40d6ca789aff0b015290faff8b2d6f0
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-12 22:21:43 -0600

    Answer #2

--- 2017/day15/src/main.rs
+++ 2017/day15/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-    let gen_a = Generator::new(703, 16807);
-    let gen_b = Generator::new(516, 48271);
+    let gen_a = Generator::new(703, 16807, 1);
+    let gen_b = Generator::new(516, 48271, 1);
 
     println!("Answer #1: {}", judge(gen_a, gen_b, 40000000));
+
+    let gen_c = Generator::new(703, 16807, 4);
+    let gen_d = Generator::new(516, 48271, 8);
+    println!("Answer #2: {}", judge(gen_c, gen_d, 5000000));
 }
 
 fn judge(generator_a: Generator, generator_b: Generator, iterations: usize) -> usize {
@@ -25,13 +29,15 @@ fn judge(generator_a: Generator, generator_b: Generator, iterations: usize) -> u
 struct Generator {
     state: usize,
     factor: usize,
+    modulo: usize,
 }
 
 impl Generator {
-    fn new(start: usize, factor: usize) -> Generator {
+    fn new(start: usize, factor: usize, modulo: usize) -> Generator {
         Generator {
             state: start,
             factor: factor,
+            modulo: modulo,
         }
     }
 }
@@ -41,7 +47,13 @@ impl Iterator for Generator {
 
     fn next(&mut self) -> Option<i16> {
         const MOD: usize = 2147483647;
-        let next = (self.state * self.factor) % MOD;
+
+        let mut next = (self.state * self.factor) % MOD;
+
+        while next % self.modulo != 0 {
+            next = (next * self.factor) % MOD;
+        }
+
         self.state = next;
 
         Some(self.state as i16)
@@ -53,16 +65,30 @@ mod tests {
     use super::*;
     #[test]
     fn test_judge_matches_small() {
-        let gen_a = Generator::new(65, 16807);
-        let gen_b = Generator::new(8921, 48271);
+        let gen_a = Generator::new(65, 16807, 1);
+        let gen_b = Generator::new(8921, 48271, 1);
 
         assert_eq!(1, judge(gen_a, gen_b, 5));
     }
     #[test]
     fn test_judge_matches_large() {
-        let gen_a = Generator::new(65, 16807);
-        let gen_b = Generator::new(8921, 48271);
+        let gen_a = Generator::new(65, 16807, 1);
+        let gen_b = Generator::new(8921, 48271, 1);
 
         assert_eq!(588, judge(gen_a, gen_b, 40000000));
+    }
+    #[test]
+    fn test_judge_matches_small_mod() {
+        let gen_a = Generator::new(65, 16807, 4);
+        let gen_b = Generator::new(8921, 48271, 8);
+
+        assert_eq!(1, judge(gen_a, gen_b, 1057));
+    }
+    #[test]
+    fn test_judge_matches_large_mod() {
+        let gen_a = Generator::new(65, 16807, 4);
+        let gen_b = Generator::new(8921, 48271, 8);
+
+        assert_eq!(309, judge(gen_a, gen_b, 5000000));
     }
 }
