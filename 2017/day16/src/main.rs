c8c94572bef7654fe93d0e730626f55a277474b0
Jack Weinbender <jack.weinbender@gmail.com>
2018-01-15 17:16:27 -0600

    Answer #2

--- 2017/day16/src/main.rs
+++ 2017/day16/src/main.rs
@@ -1,61 +1,91 @@
 #![feature(slice_rotate)]
 
-use std::str::FromStr;
-use std::string::ParseError;
-
-fn main() {
-    let mut programs: Programs = "abcdefghijklmnop".split("").filter(|&x| x != "").map(|x| String::from(x)).collect();
-    let instructions = include_str!("input.txt").split(",").map(|x| x.parse::<Instruction>()).filter_map(Result::ok);
-
-    for instruction in instructions {
-        execute_instruction(&mut programs, instruction);
-    }
-
-    println!("{:?}", programs.join(""));
-
-
-}
-
-type Program = String;
+type Program = char;
 type Index = usize;
-type Programs = Vec<Program>;
 
+fn main() -> () {
+    let instructions: Vec<Instruction> = include_str!("input.txt")
+        .split(",")
+        .filter_map(|x| parse_instruction(x))
+        .collect();
 
-enum Instruction {
-    Spin(Index),
-    Exchange(Index, Index),
-    Partner(Program, Program),
-}
+    let mut input = vec![
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+    ];
 
-impl FromStr for Instruction {
-    type Err = ParseError;
+    let loopy = 1000000000 % 60;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let string = String::from(s);
-        match &string.split_at(1) {
-            &("s", index) => { Ok( Instruction::Spin( index.parse::<usize>().unwrap() ) ) },
-            &("x", indexes) => { 
-                let is: Vec<&str> = indexes.split("/").collect();
-                Ok( Instruction::Exchange(is[0].parse::<usize>().unwrap(), is[1].parse::<usize>().unwrap()) ) },
-            &("p", programs) => {
-                let ps: Vec<&str> = programs.split("/").collect();
-                Ok( Instruction::Partner(String::from(ps[0]), String::from(ps[1]) ) )},
-            _ => { Ok(Instruction::Spin( 0 )) }
+    for i in 0..loopy {
+        dance(&mut input, &instructions);
+        if i == 0 {
+            println!(
+                "Answer #1: {:?}",
+                input.clone().into_iter().collect::<String>()
+            );
+        }
+
+        if input ==
+            vec![
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+            ]
+        {
+            println!("Loop at: {:?}", i);
         }
     }
+        println!("Answer #2: {:?}", input.clone().into_iter().collect::<String>());
 }
-
-fn execute_instruction(programs: &mut Vec<Program>, instruction: Instruction) -> () {
+fn dance(mut input: &mut Vec<Program>, instructions: &Vec<Instruction>) -> () {
+    for instr in instructions {
+        dispatch_instruction(&mut input, instr);
+    }
+}
+fn dispatch_instruction(programs: &mut Vec<Program>, instruction: &Instruction) -> () {
     match instruction {
-        Instruction::Spin(index) => spin(programs, index),
-        Instruction::Exchange(index_a, index_b) => exchange(programs, (index_a, index_b)),
-        Instruction::Partner(prgm_a, prgm_b) => partner(programs, (prgm_a, prgm_b)),
+        &Instruction::Spin(index) => {
+            spin(programs, index);
+        }
+        &Instruction::Exchange(idx_a, idx_b) => {
+            exchange(programs, (idx_a, idx_b));
+        }
+        &Instruction::Partner(pgm_a, pgm_b) => {
+            partner(programs, (pgm_a, pgm_b));
+        }
     }
 }
 
 fn spin(programs: &mut Vec<Program>, index: Index) -> () {
     let shift = programs.len() - index;
-    programs.rotate( shift );
+    programs.rotate(shift);
 }
 
 fn exchange(programs: &mut Vec<Program>, indexes: (Index, Index)) -> () {
@@ -64,31 +94,74 @@ fn exchange(programs: &mut Vec<Program>, indexes: (Index, Index)) -> () {
 }
 
 fn partner(programs: &mut Vec<Program>, partners: (Program, Program)) -> () {
-    let partner_a = programs.iter().position(|x| *x == partners.0 );
-    let partner_b = programs.iter().position(|x| *x == partners.1 );
+    let partner_a = programs.iter().position(|x| *x == partners.0);
+    let partner_b = programs.iter().position(|x| *x == partners.1);
 
     programs.swap(partner_a.unwrap(), partner_b.unwrap());
 }
 
+enum Instruction {
+    Spin(Index),
+    Exchange(Index, Index),
+    Partner(Program, Program),
+}
+
+fn parse_instruction(input: &str) -> Option<Instruction> {
+    match input.split_at(1) {
+        ("s", tail) => {
+            let index = tail.parse::<Index>().unwrap();
+            Some(Instruction::Spin(index))
+        }
+        ("x", tail) => {
+            let idxs: Vec<&str> = tail.split("/").collect();
+            let a = idxs[0].parse::<Index>().unwrap();
+            let b = idxs[1].parse::<Index>().unwrap();
+            Some(Instruction::Exchange(a, b))
+        }
+        ("p", tail) => {
+            let pgms: Vec<&str> = tail.split("/").collect();
+            let a = pgms[0].parse::<Program>().unwrap();
+            let b = pgms[1].parse::<Program>().unwrap();
+            Some(Instruction::Partner(a, b))
+        }
+        _ => None,
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_dance() {
-        let mut input = "abcde".split("").filter(|x| *x != "").map(|x| String::from(x)).collect();
-
+    fn test_spin() {
+        let mut input = vec!['a', 'b', 'c', 'd', 'e'];
         spin(&mut input, 1);
-        let expected_1: Vec<Program> = "eabcd".split("").filter(|x| *x != "").map(|x| String::from(x)).collect();
-        assert_eq!(input, expected_1);
-
+        assert_eq!(input, vec!['e', 'a', 'b', 'c', 'd']);
+    }
+    #[test]
+    fn test_exchange() {
+        let mut input = vec!['e', 'a', 'b', 'c', 'd'];
         exchange(&mut input, (3, 4));
-        let expected_2: Vec<Program> = "eabdc".split("").filter(|x| *x != "").map(|x| String::from(x)).collect();
-        assert_eq!(input, expected_2);
+        assert_eq!(input, vec!['e', 'a', 'b', 'd', 'c']);
+    }
+    #[test]
+    fn test_partners() {
+        let mut input = vec!['e', 'a', 'b', 'd', 'c'];
+        partner(&mut input, ('e', 'b'));
+        assert_eq!(input, vec!['b', 'a', 'e', 'd', 'c']);
+    }
+    #[test]
+    fn test_dispatch() {
+        let mut input = vec!['a', 'b', 'c', 'd', 'e'];
+        let instrs = vec![
+            Instruction::Spin(1),
+            Instruction::Exchange(3, 4),
+            Instruction::Partner('e', 'b'),
+        ];
 
-        partner(&mut input, (String::from("e"), String::from("b")));
-        let expected_3: Vec<Program> = "baedc".split("").filter(|x| *x != "").map(|x| String::from(x)).collect();
-        assert_eq!(input, expected_3);
+        for instr in &instrs {
+            dispatch_instruction(&mut input, instr);
+        }
 
+        assert_eq!(input, vec!['b', 'a', 'e', 'd', 'c']);
     }
 }
