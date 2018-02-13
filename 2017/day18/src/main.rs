70d3815de86e0f4f51a15a09e0fb9e608b3a5b97
Jack Weinbender <jack.weinbender@gmail.com>
2018-02-13 12:45:04 -0800

    Possible answer #2

--- 2017/day18/src/main.rs
+++ 2017/day18/src/main.rs
@@ -1,7 +1,7 @@
 
 use std::str::FromStr;
 use std::collections::HashMap;
-
+use std::collections::VecDeque;
 
 type Index = usize;
 
@@ -13,8 +13,119 @@ fn main() {
     let part_1 = Registry::new(&instructions).first_recieve();
     println!("Answer #1: {}", part_1);
 
-    // let part_2a = Registry::new(&instructions);
-    // let part_2b = Registry::new(&instructions);
+    // Part 2
+    let mut part_2a = Registry::new(&instructions);
+    let mut part_2b = Registry::new(&instructions);
+
+    part_2a.map.insert('p', 0);
+    part_2b.map.insert('p', 1);
+
+    loop {
+        part_2a.inbox.append(&mut part_2b.outbox);
+        part_2b.outbox = VecDeque::new();
+        
+        while let Some(i) = part_2a.next() {
+            // println!("PartA Index: {}", i);
+            part_2a.index = i;
+
+            /* Optimisations */
+            if part_2a.index == 4 {
+                part_2a.map.insert('a', 2147483648);
+                part_2a.map.insert('i', 0);
+                part_2a.index = 7;
+            }
+            if part_2a.index == 10 {
+                let mut p = 826;
+                let a = *part_2a.map.entry('a').or_insert(0);
+
+                for _ in 0..127 {
+                    p *= 8505;
+                    p %= a;
+                    p *= 129749;
+                    p += 12345;
+                    p %= a;
+                }
+                {
+                    let b = part_2a.map.entry('b').or_insert(0);
+                    *b = p % 10000;
+                    part_2a.last_played = *b;
+                    part_2a.send_count += 128;
+                    for _ in 0..127 {
+                        part_2a.outbox.push_back(part_2a.last_played);
+                    }
+                }
+                part_2a.map.insert('p', p);
+                part_2a.map.insert('i', 0);
+                part_2a.index = 20;
+            }
+            if part_2a.index == 27 {
+                
+                let a = part_2a.map.get(&'a').unwrap().clone();
+                let b = part_2a.map.get(&'b').unwrap().clone();
+
+                if a <= b {
+                    part_2a.outbox.push_back(a);
+                    part_2a.send_count += 1;
+                    part_2a.map.insert('a', b);
+                    part_2a.index = 36;
+                }
+            }
+        }
+        
+
+        part_2b.inbox.append(&mut part_2a.outbox);
+        part_2a.outbox = VecDeque::new();
+        
+        if let Some(j) = part_2b.next() {
+            // println!("PartB Index: {}", j);
+            part_2b.index = j;
+            /* Optimisations */
+            if part_2b.index == 4 {
+                part_2b.map.insert('a', 2147483648);
+                part_2b.map.insert('i', 0);
+                part_2b.index = 7;
+            }
+            if part_2b.index == 10 {
+                let mut p = 826;
+                let a = *part_2b.map.entry('a').or_insert(0);
+
+                for _ in 0..127 {
+                    p *= 8505;
+                    p %= a;
+                    p *= 129749;
+                    p += 12345;
+                    p %= a;
+                }
+                {
+                    let b = part_2b.map.entry('b').or_insert(0);
+                    *b = p % 10000;
+                    part_2b.last_played = *b;
+                    part_2b.send_count += 128;
+                    for _ in 0..127 {
+                        part_2b.outbox.push_back(part_2b.last_played);
+                    }
+                }
+                part_2b.map.insert('p', p);
+                part_2b.map.insert('i', 0);
+                part_2b.index = 20;
+            }
+            if part_2b.index == 27 {
+                
+                let a = part_2b.map.get(&'a').unwrap().clone();
+                let b = part_2b.map.get(&'b').unwrap().clone();
+
+                if a <= b {
+                    part_2b.outbox.push_back(a);
+                    part_2b.send_count += 1;
+                    part_2b.map.insert('a', b);
+                    part_2b.index = 36;
+                }
+            }
+        } else {
+            println!("Answer #2: {:?}", part_2a.send_count);
+            break;
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -22,9 +133,10 @@ struct Registry<'a> {
     map: HashMap<char, isize>,
     index: usize,
     last_played: isize,
+    send_count: isize,
     instr: &'a Vec<Instr>,
-    recipient: Option<&'a mut Registry<'a>>,
-    inbox: Vec<isize>,
+    outbox: VecDeque<isize>,
+    inbox: VecDeque<isize>,
 }
 
 impl<'a> Registry<'a> {
@@ -33,9 +145,10 @@ impl<'a> Registry<'a> {
             map: HashMap::new(),
             instr: instructions,
             index: 0,
+            send_count: 0,
             last_played: 0,
-            recipient: None,
-            inbox: vec![],
+            outbox: VecDeque::new(),
+            inbox: VecDeque::new(),
         }
     }
     fn next(&mut self) -> Option<Index> {
@@ -57,7 +170,7 @@ impl<'a> Registry<'a> {
                 self.modulo(val, by_val);
             }
             Instr::Receive(val) => {
-                match self.inbox.pop() {
+                match self.inbox.pop_front() {
                     Some(incoming) => {
                         self.receive(val, incoming);
                     }
@@ -73,6 +186,7 @@ impl<'a> Registry<'a> {
         }
 
         match new_index {
+            i if i == self.index as isize => None,
             i if i >= self.instr.len() as isize => None,
             i if i < 0 => None,
             i => Some(i as Index),
@@ -88,11 +202,7 @@ impl<'a> Registry<'a> {
                 self.last_played = f;
             }
         }
-
-        match self.recipient {
-            Some(ref mut reg) => reg.inbox.push(self.last_played),
-            None => {}
-        }
+        self.outbox.push_back(self.last_played);
     }
     fn receive(&mut self, register: Value, incoming: isize) {
         if let Value::Register(r) = register {
@@ -290,5 +400,8 @@ mod tests {
         let first = Registry::new(&instructions).first_recieve();
 
         assert_eq!(first, 7071);
+    }
+    #[test]
+    fn test_count_send() {
     }
 }
