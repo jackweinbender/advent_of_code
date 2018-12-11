6da01b545b959faa9ab47a0c88c49cbc2122fb91
Jack Weinbender <jack.weinbender@gmail.com>
2018-12-11 09:48:17 -0600

    fmt

--- 2018/day05/src/main.rs
+++ 2018/day05/src/main.rs
@@ -3,8 +3,8 @@ use std::collections::LinkedList;
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("Answer #1: {}", compress_polymer(input).len() );
-    println!("Answer #2: {}", get_minimal_compression(input) );
+    println!("Answer #1: {}", compress_polymer(input).len());
+    println!("Answer #2: {}", get_minimal_compression(input));
 }
 
 fn compress_polymer(input: &str) -> String {
@@ -14,13 +14,13 @@ fn compress_polymer(input: &str) -> String {
     while let Some(ch) = chars.next() {
         match s.back() {
             Some(&c) => {
-                if let Some(n) = combine(c, ch){
+                if let Some(n) = combine(c, ch) {
                     s.push_back(n);
                 } else {
                     s.pop_back();
                 }
-            },
-            None => s.push_back(ch)
+            }
+            None => s.push_back(ch),
         }
     }
     s.into_iter().collect::<String>()
@@ -29,10 +29,10 @@ fn compress_polymer(input: &str) -> String {
 fn get_candidate_polymers(input: &str) -> Vec<usize> {
     let s = compress_polymer(input);
     let mut candidates = vec![];
-    
-    for a in (0..26).map(|x| CharPair( (x + 'a' as u8) as char , (x + 'A' as u8) as char) ) {
+
+    for a in (0..26).map(|x| CharPair((x + 'a' as u8) as char, (x + 'A' as u8) as char)) {
         let mut c = s.clone().replace(a.0, "").replace(a.1, "");
-        candidates.push( compress_polymer(&c).len() )
+        candidates.push(compress_polymer(&c).len())
     }
     candidates
 }
@@ -44,8 +44,12 @@ fn get_minimal_compression(input: &str) -> usize {
 
 fn combine(a: char, b: char) -> Option<char> {
     if a.to_lowercase().to_string() == b.to_lowercase().to_string() {
-        if a.is_lowercase() && b.is_uppercase() { return None; }
-        if b.is_lowercase() && a.is_uppercase() { return None; }
+        if a.is_lowercase() && b.is_uppercase() {
+            return None;
+        }
+        if b.is_lowercase() && a.is_uppercase() {
+            return None;
+        }
     }
     Some(b)
 }
@@ -58,11 +62,11 @@ mod tests {
     #[test]
     fn test_compress_polymer() {
         let input = "dabAcCaCBAcCcaDA";
-        assert_eq!(compress_polymer(input).len(), 10 );
+        assert_eq!(compress_polymer(input).len(), 10);
     }
     #[test]
     fn test_get_candidate_polymers() {
         let input = "dabAcCaCBAcCcaDA";
-        assert_eq!(get_candidate_polymers(input).iter().min().unwrap(), &4 );
+        assert_eq!(get_candidate_polymers(input).iter().min().unwrap(), &4);
     }
 }
