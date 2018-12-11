b46870223853ac2d02cc9967e777808a89c348c1
Jack Weinbender <jack.weinbender@gmail.com>
2018-12-11 09:46:06 -0600

    fmt

--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 type Count = usize;
 
-fn main(){
+fn main() {
     let input = include_str!("input.txt");
     let box_ids = parse_input(input);
 
@@ -10,59 +10,80 @@ fn main(){
     println!("Answer #2: {}", BoxID::find_box(&box_ids).unwrap());
 }
 
-fn parse_input(input: &'static str) -> Vec<BoxID>{
-    input.lines().map(|l| BoxID{ id: l.trim() } ).collect()
+fn parse_input(input: &'static str) -> Vec<BoxID> {
+    input.lines().map(|l| BoxID { id: l.trim() }).collect()
 }
 
-struct BoxID { id: &'static str }
+struct BoxID {
+    id: &'static str,
+}
 impl BoxID {
     fn find_box(ids: &Vec<BoxID>) -> Option<String> {
         for id in ids {
             for i in ids {
                 if let Some(common) = BoxID::diff(id.id, i.id) {
-                    return Some(common)
+                    return Some(common);
                 }
             }
         }
         None
     }
-    fn diff(a: &str, b: & str) -> Option<String> {
+    fn diff(a: &str, b: &str) -> Option<String> {
         let mut diffs = 0;
         let mut common = String::new();
         let mut a_iter = a.chars();
         let mut b_iter = b.chars();
         while let Some(ch_a) = a_iter.next() {
             if let Some(ch_b) = b_iter.next() {
-                if diffs > 1 { break; }
+                if diffs > 1 {
+                    break;
+                }
                 if ch_a == ch_b {
                     common.push(ch_a);
-                } else  { 
+                } else {
                     diffs += 1;
                 }
             }
         }
-        if diffs == 1 { Some(common) }
-        else { None }
+        if diffs == 1 {
+            Some(common)
+        } else {
+            None
+        }
     }
     fn checksum(ids: &Vec<BoxID>) -> usize {
-        let (twos, threes) = ids.iter().map(|b| BoxID::check(b) ).fold((0,0), 
-            |(mut a,mut b), (x,y)| {
-                if x >= 1 { a += 1 }
-                if y >= 1 { b += 1 }
+        let (twos, threes) =
+            ids.iter()
+                .map(|b| BoxID::check(b))
+                .fold((0, 0), |(mut a, mut b), (x, y)| {
+                    if x >= 1 {
+                        a += 1
+                    }
+                    if y >= 1 {
+                        b += 1
+                    }
 
-                ( a, b )
-            });
+                    (a, b)
+                });
         twos * threes
     }
 
-    fn check(box_id: &BoxID) -> ( Count, Count ) {
+    fn check(box_id: &BoxID) -> (Count, Count) {
         let mut letter_counter = HashMap::new();
         for ch in box_id.id.chars() {
             let counter = letter_counter.entry(ch).or_insert(0);
             *counter += 1;
         }
-        let twos = letter_counter.values().filter(|v| **v == 2).collect::<Vec<&usize>>().len();
-        let threes = letter_counter.values().filter(|v| **v == 3).collect::<Vec<&usize>>().len();
+        let twos = letter_counter
+            .values()
+            .filter(|v| **v == 2)
+            .collect::<Vec<&usize>>()
+            .len();
+        let threes = letter_counter
+            .values()
+            .filter(|v| **v == 3)
+            .collect::<Vec<&usize>>()
+            .len();
 
         (twos, threes)
     }
@@ -76,10 +97,10 @@ mod tests {
         let id_1 = BoxID { id: "abcdef" };
         let id_2 = BoxID { id: "bababc" };
         let id_3 = BoxID { id: "abbcde" };
- 
-        assert_eq!(BoxID::check(&id_1), (0,0) );
-        assert_eq!(BoxID::check(&id_2), (1,1) );
-        assert_eq!(BoxID::check(&id_3), (1,0) );
+
+        assert_eq!(BoxID::check(&id_1), (0, 0));
+        assert_eq!(BoxID::check(&id_2), (1, 1));
+        assert_eq!(BoxID::check(&id_3), (1, 0));
     }
     #[test]
     fn test_checksum() {
