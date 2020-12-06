6204440873030b6869930f1fe6aaebee849ea20d
Jack Weinbender <jack.weinbender@gmail.com>
2020-12-06 11:37:13 -0600

    Part 1 refactor

--- 2020/src/day04/index.ts
+++ 2020/src/day04/index.ts
@@ -1,52 +1,62 @@
 import { test, readInput } from "../utils/index";
 
+// Input Prep
+
 const prepareInput = (rawInput: string) =>
   rawInput
     .split("\n\n")
     .map((l) => l.trim())
     .map(splitFields);
 const splitFields = (block) =>
-  block
-    .split(/\s+/)
-    .map((l) => l.trim())
-    .map(splitKVP);
+  new Map(
+    block
+      .split(/\s+/)
+      .map((l) => l.trim())
+      .map(splitKVP)
+  );
 const splitKVP = (entry) => entry.split(":").map((l) => l.trim());
-
 const input = prepareInput(readInput());
 
-const requiredFields = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
-const isValid = (req: string[], given: string[]): boolean =>
-  req.every((f) => given.includes(f));
-const sumValid = (req: string[]) => (acc, next) => {
-  const valid = isValid(req, next);
-  return valid ? (acc += 1) : acc;
+// Business Logic
+const isValid = (
+  req: Map<string, Function>,
+  given: Map<string, string>
+): boolean => {
+  const reqFields = Array.from(req.keys());
+
+  return reqFields.every((f) => {
+    const validatorFn = req.get(f);
+    const value = given.get(f);
+    return validatorFn(value);
+  });
 };
 
+const sumValid = (req: Map<string, Function>) => (acc, next) =>
+  isValid(req, next) ? (acc += 1) : acc;
+
+// Part A Policy
+const policy_one: Map<string, Function> = new Map([
+  ["byr", (x) => (x ? true : false)],
+  ["iyr", (x) => (x ? true : false)],
+  ["eyr", (x) => (x ? true : false)],
+  ["hgt", (x) => (x ? true : false)],
+  ["hcl", (x) => (x ? true : false)],
+  ["ecl", (x) => (x ? true : false)],
+  ["pid", (x) => (x ? true : false)],
+  ["cid", (x) => true],
+]);
+
 const goA = (input) => {
-  return input
-    .map((p) => p.map((e) => e[0]))
-    .reduce(sumValid(requiredFields), 0);
+  return input.reduce(sumValid(policy_one), 0);
 };
+
+// Part B Policy
 
 const goB = (input) => {
   return;
 };
 
 /* Tests */
-
-test(
-  isValid(requiredFields, [
-    "ecl",
-    "pid",
-    "eyr",
-    "hcl",
-    "byr",
-    "iyr",
-    "cid",
-    "hgt",
-  ]),
-  true
-);
 
 const testInput = `
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -63,7 +73,6 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
 `;
-
 test(goA(prepareInput(testInput)), 2);
 
 /* Results */
